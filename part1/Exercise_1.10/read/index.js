1c49f55b81b9842b11585a3b8b9391ba926ef316
Ville Korppi <ville.a.korppi@gmail.com>
2022-06-22 16:07:51 +0300

    debugging

--- part1/Exercise_1.10/read/index.js
+++ part1/Exercise_1.10/read/index.js
@@ -5,18 +5,22 @@ const http = require('http')
 const cron = require('node-cron');
 const server = http.createServer(app)
 const fs = require('fs')
-const path="/usr/src/logs/timestamp/"
+const path="/usr/src/generator/timestamp"
 
 const portEnv = process.env.PORT || 3012
 
 cron.schedule('*/5 * * * * *', async () => {
 
 
+   console.log("read "+path+'time.txt')
+
    fs.readFile(path+'time.txt', 'utf8', (readError, data) => {
      
       console.log((readError ? readError : data+': '+generatedstring) )
 
     });
+
+    console.log("success")
 
 });
 
--- part1/Exercise_1.10/write/index.js
+++ part1/Exercise_1.10/write/index.js
@@ -2,12 +2,17 @@
 require('dotenv').config()
 const cron = require('node-cron');
 const fs = require('fs')
-const path="/usr/src/logs/timestamp/"
+const path="/usr/src/generator/timestamp"
 
 cron.schedule('*/5 * * * * *', async () => {
+
+   console.log("write "+path+'time.txt')
    
    const time = new Date(Date.now())+''
 
+   console.log("write")
    fs.writeFile(path+'time.txt',time, writeError => {})
+
+   console.log("success")
    
 });
