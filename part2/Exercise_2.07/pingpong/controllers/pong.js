dc134ab272b5f1c184e39963e2394c088e91bb55
Ville Korppi <ville.a.korppi@gmail.com>
2022-06-26 23:50:23 +0300

    database test

--- part2/Exercise_2.07/pingpong/controllers/pong.js
+++ part2/Exercise_2.07/pingpong/controllers/pong.js
@@ -2,8 +2,17 @@
 
 const routerPong = require('express').Router()
 const fs = require('fs')
+const { Client } = require('pg')
 const path="/usr/src/pingpong/howmanypings/"
 var counter=0
+
+const client = new Client({
+      user: 'testuser',
+      host: 'database',
+      database: 'test',
+      password: 'password',
+      port: 5432,
+    })
 
 routerPong.get('/',async(request, response) => {
 
@@ -35,6 +44,16 @@ routerPong.get('/count',async(request, response) => {
      
       response.write(counter+'', 'utf8', () => {});
       response.end('')
+	  
+})
+
+routerPong.get('/testdb',async(request, response) => {
+
+      client.connect()
+
+      const res = await client.query('SELECT 1')
+      console.log('Debug: '+res.rows[0])
+      await client.end()
 	  
 })
 
